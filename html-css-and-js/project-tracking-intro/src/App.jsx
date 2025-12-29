20837c7ca7fa2688c8bef73073fb55cbf9ec90e7
emmanueltn007 <emmanuelnotyhawe@gmail.com>
2025-12-29 21:48:18 +0200

    Changed the background color

--- html-css-and-js/project-tracking-intro/src/App.jsx
+++ html-css-and-js/project-tracking-intro/src/App.jsx
@@ -4,7 +4,7 @@ import Hero from "./components/Hero";
 function App () {
   return (
     <div className="min-h-screen min-w-screen px-8 py-12 md:px-12 md:py-16 overflow-x-hidden relative">
-      <div className="absolute top-0 right-0 bg-gray-500 h-96 w-1/2 rounded-bl-4xl -z-10"></div>
+      <div className="absolute top-0 right-0 bg-gray-200 h-96 w-1/2 rounded-bl-4xl -z-10"></div>
       <Header />
       <Hero />
     </div>
